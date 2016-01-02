e948bd6825ee1de176b24e513610b2452a62b903
XCL <80213876@qq.com>
2016-01-02 11:49:11 +0800

    add header

--- XCLNetTools/EasyUI/Model/TreeItem.cs
+++ XCLNetTools/EasyUI/Model/TreeItem.cs
@@ -1,4 +1,25 @@
-﻿using Newtonsoft.Json;
+﻿/*
+一：基本信息：
+开源协议：https://github.com/xucongli1989/XCLNetTools/blob/master/LICENSE
+项目地址：https://github.com/xucongli1989/XCLNetTools
+Create By: XCL @ 2012
+
+二：贡献者：
+1：xucongli1989（https://github.com/xucongli1989）电子邮件：80213876@qq.com
+
+三：更新：
+当前版本：v2.1
+更新时间：2016-01-01
+
+四：更新内容：
+1：将原先基础数据转换方法转移到Common/DataTypeConvert下面
+2：其它逻辑优化，如表单参数获取等
+3：首次开放所有源代码
+ */
+
+
+
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 
--- XCLNetTools/StringHander/Common.cs
+++ XCLNetTools/StringHander/Common.cs
@@ -1,4 +1,25 @@
-﻿using System;
+﻿/*
+一：基本信息：
+开源协议：https://github.com/xucongli1989/XCLNetTools/blob/master/LICENSE
+项目地址：https://github.com/xucongli1989/XCLNetTools
+Create By: XCL @ 2012
+
+二：贡献者：
+1：xucongli1989（https://github.com/xucongli1989）电子邮件：80213876@qq.com
+
+三：更新：
+当前版本：v2.1
+更新时间：2016-01-01
+
+四：更新内容：
+1：将原先基础数据转换方法转移到Common/DataTypeConvert下面
+2：其它逻辑优化，如表单参数获取等
+3：首次开放所有源代码
+ */
+
+
+
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.IO.Compression;
@@ -60,7 +81,7 @@ namespace XCLNetTools.StringHander
         /// </summary>
         public static string GetIpByIP138()
         {
-            string url = "http://iframe.ip138.com/ic.asp";
+            string url = "http://1212.ip138.com/ic.asp";
             string ip = string.Empty;
             XCLNetTools.Http.HttpHelper http = new Http.HttpHelper();
             try
@@ -70,14 +91,14 @@ namespace XCLNetTools.StringHander
                     URL = url,
                     Method = "get"
                 });
-                string html = result.Html;
+                string html = result.Html;//如：您的IP是：[112.13.32.16] 来自：浙江省绍兴市 移动
                 if (!string.IsNullOrEmpty(html))
                 {
-                    HtmlAgilityPack.HtmlDocument doc = new HtmlAgilityPack.HtmlDocument();
-                    doc.LoadHtml(html);
-                    ip = doc.DocumentNode.SelectSingleNode("//center").InnerText;//如：您的IP是：[112.13.32.16] 来自：浙江省绍兴市 移动
-                    Regex reg = new Regex(@"\[.*\]");
-                    ip = Regex.Replace(reg.Match(ip).Value, @"[\[\]]", "");//如：112.13.32.16
+                    Regex reg = new Regex(@"您的IP是：\[(.*)\]");
+                    if (reg.IsMatch(html))
+                    {
+                        ip = reg.Match(html).Groups[1].Value.ToUpper().Trim();//如：112.13.32.16
+                    }
                 }
             }
             catch { }
