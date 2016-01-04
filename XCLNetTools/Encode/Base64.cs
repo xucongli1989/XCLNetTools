5907cd29227116838a3597f51bd2f9e7cf4e4976
XCL <80213876@qq.com>
2016-01-04 21:47:02 +0800

    修复一处未dispose问题

--- XCLNetTools/Encode/Base64.cs
+++ XCLNetTools/Encode/Base64.cs
@@ -32,22 +32,22 @@ namespace XCLNetTools.Encode
         /// <summary>
         /// Base64加密
         /// </summary>
-        /// <param name="Message"></param>
-        /// <returns></returns>
-        public static string Base64Code(string Message)
+        /// <param name="msg">要加密的内容</param>
+        /// <returns>加密后的值</returns>
+        public static string Base64Code(string msg)
         {
-            byte[] bytes = Encoding.Default.GetBytes(Message);
+            byte[] bytes = Encoding.Default.GetBytes(msg);
             return Convert.ToBase64String(bytes);
         }
 
         /// <summary>
         /// Base64解密
         /// </summary>
-        /// <param name="Message"></param>
-        /// <returns></returns>
-        public static string Base64Decode(string Message)
+        /// <param name="msg">要解密的内容</param>
+        /// <returns>解密后的值</returns>
+        public static string Base64Decode(string msg)
         {
-            byte[] bytes = Convert.FromBase64String(Message);
+            byte[] bytes = Convert.FromBase64String(msg);
             return Encoding.Default.GetString(bytes);
         }
     }
