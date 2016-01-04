206a75227abe4aaa9f05389615a222432a1030fa
xucl <xucl@Ctrip.com>
2016-01-04 16:56:29 +0800

    更改Message/JsonMsg类的目录

---
+++ XCLNetTools/Entity/Message/JsonMsgBody.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace XCLNetTools.Entity.Message
+{
+    /// <summary>
+    /// json消息正文信息
+    /// </summary>
+    [Serializable]
+    public class JsonMsgBody
+    {
+        /// <summary>
+        /// 主数据
+        /// </summary>
+        public object Data { get; set; }
+
+        /// <summary>
+        /// 扩展数据
+        /// </summary>
+        public object ExtendData { get; set; }
+    }
+}
