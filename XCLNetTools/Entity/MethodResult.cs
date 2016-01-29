aca8ea84d19e048a580e987a84c2814b4f534b3b
xucl <xucl@Ctrip.com>
2016-01-29 18:58:02 +0800

    更新MethodResult.cs

--- XCLNetTools/Entity/MethodResult.cs
+++ XCLNetTools/Entity/MethodResult.cs
@@ -21,11 +21,13 @@ namespace XCLNetTools.Entity
 {
     /// <summary>
     /// 方法返回值实体，主要是方便一个方法输出多个信息时使用，同时也减少使用out返回结果信息
+    /// <typeparam name="T">方法返回的结果类型</typeparam>
     /// </summary>
     [Serializable]
     public class MethodResult<T>
     {
         private bool _isSuccess = true;
+        private T _result = default(T);
 
         /// <summary>
         /// 该方法执行的逻辑是否成功（默认为true）
@@ -44,6 +46,29 @@ namespace XCLNetTools.Entity
         /// <summary>
         /// 该方法返回的结果
         /// </summary>
-        public T Result { get; set; }
+        public T Result
+        {
+            get
+            {
+                if (this._result is bool)
+                {
+                    throw new Exception("当Result为bool类型时，请读取IsSuccess属性即可！");
+                }
+                return this._result;
+            }
+            set
+            {
+                if (this._result is bool)
+                {
+                    throw new Exception("当Result为bool类型时，请设置IsSuccess属性即可！");
+                }
+                this._result = value;
+            }
+        }
+
+        /// <summary>
+        /// 其它数据（比如用dictionary存放不同的数据结果）
+        /// </summary>
+        public object Data { get; set; }
     }
 }
