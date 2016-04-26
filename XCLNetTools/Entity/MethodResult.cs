642f2e024246129052efe172c7fe8231495386b2
xucongli1989 <80213876@qq.com>
2016-04-26 09:47:17 +0800

    update MethodResult

--- XCLNetTools/Entity/MethodResult.cs
+++ XCLNetTools/Entity/MethodResult.cs
@@ -12,13 +12,13 @@ namespace XCLNetTools.Entity
 {
     /// <summary>
     /// 方法返回值实体，主要是方便一个方法输出多个信息时使用，同时也减少使用out返回结果信息
-    /// <typeparam name="T">方法返回的结果类型</typeparam>
     /// </summary>
+    /// <typeparam name="TResult">方法返回的结果类型</typeparam>
     [Serializable]
-    public class MethodResult<T>
+    public class MethodResult<TResult>
     {
         private bool _isSuccess = true;
-        private T _result = default(T);
+        private TResult _result = default(TResult);
 
         /// <summary>
         /// 该方法执行的逻辑是否成功（默认为true）
@@ -37,7 +37,7 @@ namespace XCLNetTools.Entity
         /// <summary>
         /// 该方法返回的结果
         /// </summary>
-        public T Result
+        public TResult Result
         {
             get
             {
@@ -61,5 +61,59 @@ namespace XCLNetTools.Entity
         /// 其它数据（比如用dictionary存放不同的数据结果）
         /// </summary>
         public object Data { get; set; }
+    }
+
+    /// <summary>
+    /// 方法返回值实体，主要是方便一个方法输出多个信息时使用，同时也减少使用out返回结果信息
+    /// </summary>
+    /// <typeparam name="TResult">方法返回的结果类型</typeparam>
+    /// <typeparam name="TData">Data属性的类型</typeparam>
+    [Serializable]
+    public class MethodResult<TResult, TData>
+    {
+        private bool _isSuccess = true;
+        private TResult _result = default(TResult);
+
+        /// <summary>
+        /// 该方法执行的逻辑是否成功（默认为true）
+        /// </summary>
+        public bool IsSuccess
+        {
+            get { return this._isSuccess; }
+            set { this._isSuccess = value; }
+        }
+
+        /// <summary>
+        /// 该方法返回的消息提示
+        /// </summary>
+        public string Message { get; set; }
+
+        /// <summary>
+        /// 该方法返回的结果
+        /// </summary>
+        public TResult Result
+        {
+            get
+            {
+                if (this._result is bool)
+                {
+                    throw new Exception("当Result为bool类型时，请读取IsSuccess属性即可！");
+                }
+                return this._result;
+            }
+            set
+            {
+                if (this._result is bool)
+                {
+                    throw new Exception("当Result为bool类型时，请设置IsSuccess属性即可！");
+                }
+                this._result = value;
+            }
+        }
+
+        /// <summary>
+        /// 其它数据（比如用dictionary存放不同的数据结果）
+        /// </summary>
+        public TData Data { get; set; }
     }
 }
