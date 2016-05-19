59a3f70c502589e514d88cc3fdf4ae4b7d6d37fc
XCL <80213876@qq.com>
2016-05-19 20:01:25 +0800

    update Message

--- XCLNetTools/Message/MessageModel.cs
+++ XCLNetTools/Message/MessageModel.cs
@@ -19,67 +19,67 @@ namespace XCLNetTools.Message
         /// <summary>
         /// 提示标题
         /// </summary>
-        public string Title { get; set; }
+        public virtual string Title { get; set; }
 
         /// <summary>
         /// 消息提示时间
         /// </summary>
-        public DateTime? Date { get; set; }
+        public virtual DateTime? Date { get; set; }
 
         /// <summary>
         /// 消息提示内容
         /// </summary>
-        public string Message { get; set; }
+        public virtual string Message { get; set; }
 
         /// <summary>
         /// 消息详细信息
         /// </summary>
-        public string MessageMore { get; set; }
+        public virtual string MessageMore { get; set; }
 
         /// <summary>
         /// 消息发生页地址
         /// </summary>
-        public string Url { get; set; }
+        public virtual string Url { get; set; }
 
         /// <summary>
         /// 消息页来源地址(reffer)
         /// </summary>
-        public string FromUrl { get; set; }
+        public virtual string FromUrl { get; set; }
 
         /// <summary>
         /// 是否成功与失败的标识
         /// </summary>
-        public bool IsSuccess { get; set; }
+        public virtual bool IsSuccess { get; set; }
 
         /// <summary>
         /// 是否需要刷新
         /// </summary>
-        public bool IsRefresh { get; set; }
+        public virtual bool IsRefresh { get; set; }
 
         /// <summary>
         /// 备注信息
         /// </summary>
-        public string Remark { get; set; }
+        public virtual string Remark { get; set; }
 
         /// <summary>
         /// 是否需要跳转
         /// </summary>
-        public bool IsRedirect { get; set; }
+        public virtual bool IsRedirect { get; set; }
 
         /// <summary>
         /// 要跳转的url
         /// </summary>
-        public string RedirectURL { get; set; }
+        public virtual string RedirectURL { get; set; }
 
         /// <summary>
         /// 跳转方式
         /// </summary>
-        public XCLNetTools.Enum.CommonEnum.RedirectTargetEnum RedirectTarget { get; set; }
+        public virtual XCLNetTools.Enum.CommonEnum.RedirectTargetEnum RedirectTarget { get; set; }
 
         /// <summary>
         /// 当前请求是否为ajax请求
         /// </summary>
-        public bool IsAjax
+        public virtual bool IsAjax
         {
             get
             {
@@ -90,11 +90,11 @@ namespace XCLNetTools.Message
         /// <summary>
         /// 错误码
         /// </summary>
-        public string ErrorCode { get; set; }
+        public virtual string ErrorCode { get; set; }
 
         /// <summary>
         /// 自定义输出对象
         /// </summary>
-        public object CustomObject { get; set; }
+        public virtual object CustomObject { get; set; }
     }
 }
