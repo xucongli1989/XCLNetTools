b6d438439be69041dc79b8e56239046aa7d761ee
XCL <80213876@qq.com>
2016-05-21 15:11:06 +0800

    添加JObject转Url参数方法

--- XCLNetTools/Entity/PagerInfo.cs
+++ XCLNetTools/Entity/PagerInfo.cs
@@ -7,12 +7,15 @@ Create By: XCL @ 2012
  */
 
 using System;
+using System.Runtime.Serialization;
 
 namespace XCLNetTools.Entity
 {
     /// <summary>
     /// 分页信息model
     /// </summary>
+    [Serializable]
+    [DataContract]
     public class PagerInfo
     {
         private int _pageIndex = 0;
@@ -60,6 +63,7 @@ namespace XCLNetTools.Entity
         /// 当前页码（第一页为1），默认为1
         /// 注：若重新设置此值，则将一同更新其它相关联的属性值
         /// </summary>
+        [DataMember]
         public int PageIndex
         {
             get { return this._pageIndex; }
@@ -74,6 +78,7 @@ namespace XCLNetTools.Entity
         /// 每页最多显示的记录数，默认为10
         /// 注：若重新设置此值，则将一同更新其它相关联的属性值
         /// </summary>
+        [DataMember]
         public int PageSize
         {
             get { return this._pageSize; }
@@ -88,6 +93,7 @@ namespace XCLNetTools.Entity
         /// 当前记录总数
         /// 注：若重新设置此值，则将一同更新其它相关联的属性值
         /// </summary>
+        [DataMember]
         public int RecordCount
         {
             get { return this._recordCount; }
@@ -101,31 +107,101 @@ namespace XCLNetTools.Entity
         /// <summary>
         /// 当前总页数
         /// </summary>
+        [DataMember]
         public int PageCount { get; private set; }
 
         /// <summary>
         /// 当前页的实际记录条数
         /// </summary>
+        [DataMember]
         public int CurrentPageRecordCount { get; private set; }
 
         /// <summary>
         /// 当前页的第一条记录的序号
         /// </summary>
+        [DataMember]
         public int StartIndex { get; private set; }
 
         /// <summary>
         /// 当前页的最后一条记录的序号
         /// </summary>
+        [DataMember]
         public int EndIndex { get; private set; }
 
         /// <summary>
         /// controller
         /// </summary>
+        [DataMember]
         public string ControllerName { get; set; }
 
         /// <summary>
         /// action
         /// </summary>
+        [DataMember]
         public string ActionName { get; set; }
+
+        /// <summary>
+        /// 转为PagerInfoSimple对象
+        /// </summary>
+        /// <returns>PagerInfoSimple对象</returns>
+        public PagerInfoSimple ToPagerInfoSimple()
+        {
+            return new PagerInfoSimple(this.PageIndex, this.PageSize, this.RecordCount);
+        }
+    }
+
+    /// <summary>
+    /// 分页信息简易model
+    /// </summary>
+    [Serializable]
+    [DataContract]
+    public class PagerInfoSimple
+    {
+        /// <summary>
+        /// 默认构造
+        /// </summary>
+        public PagerInfoSimple()
+        {
+        }
+
+        /// <summary>
+        /// 构造方法
+        /// </summary>
+        /// <param name="pageIndex">当前为第几页</param>
+        /// <param name="pageSize">每页最多显示多少条记录</param>
+        /// <param name="recordCount">记录总数</param>
+        public PagerInfoSimple(int pageIndex, int pageSize, int recordCount)
+        {
+            this.PageIndex = pageIndex;
+            this.PageSize = pageSize;
+            this.RecordCount = recordCount;
+        }
+
+        /// <summary>
+        /// 当前为第几页
+        /// </summary>
+        [DataMember]
+        public int PageIndex { get; set; }
+
+        /// <summary>
+        /// 每页最多显示多少条记录
+        /// </summary>
+        [DataMember]
+        public int PageSize { get; set; }
+
+        /// <summary>
+        /// 记录总数
+        /// </summary>
+        [DataMember]
+        public int RecordCount { get; set; }
+
+        /// <summary>
+        /// 转为PagerInfo对象
+        /// </summary>
+        /// <returns>PagerInfo对象</returns>
+        public PagerInfo ToPagerInfo()
+        {
+            return new PagerInfo(this.PageIndex, this.PageSize, this.RecordCount);
+        }
     }
 }
--- XCLNetTools/Serialize/Lib.cs
+++ XCLNetTools/Serialize/Lib.cs
@@ -6,8 +6,11 @@ Create By: XCL @ 2012
 
  */
 
+using Newtonsoft.Json.Linq;
 using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Runtime.Serialization;
 using System.Runtime.Serialization.Formatters.Binary;
 
@@ -72,5 +75,91 @@ namespace XCLNetTools.Serialize
         }
 
         #endregion 序列化方式的深度克隆对象
+
+        #region JObject 相关
+
+        /// <summary>
+        /// 将JObject的属性填充至指定的dictionary
+        /// </summary>
+        /// <param name="result">结果</param>
+        /// <param name="p">JObject的属性</param>
+        private static void JObjectFillDictionary(Dictionary<string, string> result, JProperty p)
+        {
+            if (null == p)
+            {
+                return;
+            }
+            var ps = p.Values();
+            if (null == ps)
+            {
+                return;
+            }
+            var valCount = ps.Count();
+            if (valCount == 1)
+            {
+                result.Add(p.Path, p.Value.ToString());
+            }
+            else if (valCount > 1)
+            {
+                foreach (var m in p.Values())
+                {
+                    JObjectFillDictionary(result, (JProperty)m);
+                }
+            }
+        }
+
+        /// <summary>
+        /// 将JObject类型的属性转换为dictionary
+        /// </summary>
+        /// <returns>dictionary结果</returns>
+        public static Dictionary<string, string> ConvertJObjectToDictionary(JObject obj)
+        {
+            if (null == obj)
+            {
+                return null;
+            }
+            Dictionary<string, string> dic = new Dictionary<string, string>();
+            var pros = obj.Properties();
+            foreach (var m in pros)
+            {
+                JObjectFillDictionary(dic, m);
+            }
+            return dic;
+        }
+
+        /// <summary>
+        /// 将JObject的属性转换为url参数形式
+        /// 如：{"a":"1","b":"2","c":{"d":"100"}} -> a=1&amp;b=2&amp;c[d]=100
+        /// </summary>
+        /// <param name="obj">需要转换的对象</param>
+        /// <returns>url参数字符串</returns>
+        public static string ConvertJObjectToUrlParameters(JObject obj)
+        {
+            var dic = ConvertJObjectToDictionary(obj);
+            if (null == dic || dic.Count == 0)
+            {
+                return null;
+            }
+            List<string> str = new List<string>(dic.Count);
+            string tempKey = string.Empty;
+            for (int i = 0; i < dic.Count; i++)
+            {
+                var d = dic.ElementAt(i);
+                if (d.Key.IndexOf('.') >= 0)
+                {
+                    tempKey = (d.Key.Replace(".", "][") + "]");
+                    tempKey = tempKey.Remove(tempKey.IndexOf(']'), 1);
+                    str.Add(string.Format("{0}={1}", tempKey, System.Web.HttpUtility.UrlEncode(d.Value)));
+                }
+                else
+                {
+                    str.Add(string.Format("{0}={1}", d.Key, System.Web.HttpUtility.UrlEncode(d.Value)));
+                }
+            }
+
+            return string.Join("&", str);
+        }
+
+        #endregion JObject 相关
     }
 }
