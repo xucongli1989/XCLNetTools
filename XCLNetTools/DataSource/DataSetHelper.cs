c10e93a9ed6247dfb2d42032a6dbd85aab3e1ce9
XCL <80213876@qq.com>
2016-12-15 21:26:35 +0800

    add DataReaderHelper

--- XCLNetTools/DataSource/DataSetHelper.cs
+++ XCLNetTools/DataSource/DataSetHelper.cs
@@ -1,7 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using System.Collections.Generic;
+using System.Data;
 
 namespace XCLNetTools.DataSource
 {
@@ -10,5 +8,44 @@ namespace XCLNetTools.DataSource
     /// </summary>
     public class DataSetHelper
     {
+        /// <summary>
+        /// 将dataset的第一个datatable转为list
+        /// </summary>
+        /// <param name="ds">要转换的数据</param>
+        /// <returns>list</returns>
+        public static IList<T> DataSetToIList<T>(DataSet ds) where T : new()
+        {
+            return XCLNetTools.Generic.ListHelper.DataSetToList<T>(ds);
+        }
+
+        /// <summary>
+        /// 将dataTable转为list
+        /// </summary>
+        /// <param name="dt">要转换的数据</param>
+        /// <returns>list</returns>
+        public static IList<T> DataTableToIList<T>(DataTable dt) where T : new()
+        {
+            return XCLNetTools.Generic.ListHelper.DataTableToList<T>(dt);
+        }
+
+        /// <summary>
+        /// 将dataset的第一个datatable转为list
+        /// </summary>
+        /// <param name="ds">要转换的数据</param>
+        /// <returns>list</returns>
+        public static List<T> DataSetToList<T>(DataSet ds) where T : new()
+        {
+            return XCLNetTools.Generic.ListHelper.DataSetToList<T>(ds) as List<T>;
+        }
+
+        /// <summary>
+        /// 将dataTable转为list
+        /// </summary>
+        /// <param name="dt">要转换的数据</param>
+        /// <returns>list</returns>
+        public static List<T> DataTableToList<T>(DataTable dt) where T : new()
+        {
+            return XCLNetTools.Generic.ListHelper.DataTableToList<T>(dt) as List<T>;
+        }
     }
 }
