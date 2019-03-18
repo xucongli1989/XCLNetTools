49627a98b2e968f842a35bd9755a64098f70a00e
xucongli1989 <80213876@qq.com>
2019-03-18 19:21:21 +0800

    add TreeDataHelper

---
+++ XCLNetTools/Entity/TreeData/TreeMapEntity.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace XCLNetTools.Entity.TreeData
+{
+    /// <summary>
+    /// 树状层级映射实体
+    /// </summary>
+    /// <typeparam name="IDType">主键数据类型</typeparam>
+    /// <typeparam name="ModelType">要查询的记录实体类型</typeparam>
+    [Serializable]
+    public class TreeMapEntity<IDType, ModelType> : ExtendDataEntity<IDType, ModelType>
+    {
+        /// <summary>
+        /// 子数据实体
+        /// </summary>
+        public List<TreeMapEntity<IDType, ModelType>> ChildList { get; set; }
+    }
+}
