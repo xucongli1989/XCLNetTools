c765ec4561091b65f06d8522e2b156352b49922b
xucongli1989 <80213876@qq.com>
2020-09-27 18:17:25 +0800

    update ip query method.

--- XCLNetTools/Common/IPHelper.cs
+++ XCLNetTools/Common/IPHelper.cs
@@ -56,38 +56,35 @@ namespace XCLNetTools.Common
         /// </summary>
         public static XCLNetTools.Entity.LocationEntity GetIPFromPublicWeb()
         {
-            var html = "";
             var model = new XCLNetTools.Entity.LocationEntity();
             var http = new Http.HttpHelper();
+            dynamic ipResult = null;
             try
             {
                 var result = http.GetHtml(new XCLNetTools.Entity.Http.HttpItem()
                 {
-                    URL = "https://www.ip.cn/",
+                    URL = "https://www.ip.cn/api/index?ip=&type=0",
                     Method = "get",
-                    UserAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.100 Safari/537.36"
+                    UserAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.100 Safari/537.36",
+                    Timeout = 10000
                 });
-                html = result.Html;//如：<div class="well"><p>您现在的 IP：<code>113.118.186.219</code></p><p>所在地理位置：<code>广东省深圳市 电信</code></p><p>GeoIP: <code>Shenzhen, Guangdong, China</code></p></div>
+                //如：{"rs":1,"code":0,"address":"中国  广东省 深圳市 电信","ip":"113.87.183.231","isDomain":0}
+                var html = result.Html;
+                if (!string.IsNullOrWhiteSpace(html))
+                {
+                    ipResult = Newtonsoft.Json.JsonConvert.DeserializeObject(html);
+                }
             }
             catch
             {
                 //
             }
-            if (string.IsNullOrWhiteSpace(html))
+            if (ipResult is null)
             {
                 return model;
             }
-            var reg = new Regex(@"您现在的\s+IP：<code>(.+?)</code>.*所在地理位置：<code>(.+?)</code>");
-            if (!reg.IsMatch(html))
-            {
-                return model;
-            }
-            var groups = reg.Match(html).Groups;
-            if (groups.Count == 3)
-            {
-                model.IP = groups[1].Value.ToUpper().Trim();//如：113.118.186.219
-                model.Address = groups[2].Value.Trim();//如：广东省深圳市 电信
-            }
+            model.IP = ipResult.ip;
+            model.Address = ipResult.address;
             return model;
         }
 
