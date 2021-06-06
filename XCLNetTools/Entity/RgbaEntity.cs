af4e53961e7f99e5b1326b6b7a2a3ab3a604ccbb
xucongli1989 <80213876@qq.com>
2021-06-06 19:47:23 +0800

    添加新的公共实体类

---
+++ XCLNetTools/Entity/RgbaEntity.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XCLNetTools.Entity
+{
+    /// <summary>
+    /// R、G、B、A 实体
+    /// </summary>
+    [Serializable]
+    public class RgbaEntity
+    {
+        /// <summary>
+        /// 0-255
+        /// </summary>
+        public byte R { get; set; }
+
+        /// <summary>
+        /// 0-255
+        /// </summary>
+        public byte G { get; set; }
+
+        /// <summary>
+        /// 0-255
+        /// </summary>
+        public byte B { get; set; }
+
+        /// <summary>
+        /// 0-1
+        /// </summary>
+        public double A { get; set; }
+    }
+}
