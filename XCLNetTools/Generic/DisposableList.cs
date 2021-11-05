a87aaa6b11bd0328c6ad18fa3be9626487ce9daa
xucongli1989 <80213876@qq.com>
2021-11-06 07:39:42 +0800

    add DisposableList

---
+++ XCLNetTools/Generic/DisposableList.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace XCLNetTools.Generic
+{
+    /// <summary>
+    /// 带有 Dispose 的 List
+    /// </summary>
+    public class DisposableList : List<IDisposable>, IDisposable
+    {
+        /// <summary>
+        /// 释放内存
+        /// </summary>
+        public void Dispose()
+        {
+            this.ForEach(x =>
+            {
+                try
+                {
+                    x.Dispose();
+                }
+                catch
+                {
+                    //
+                }
+            });
+        }
+    }
+}
