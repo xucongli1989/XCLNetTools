6d643eabd203a7461ded9904e02103db3cf72e95
xucongli1989 <80213876@qq.com>
2021-11-07 10:25:26 +0800

    DisposableList 改为泛型

--- XCLNetTools/Generic/DisposableList.cs
+++ XCLNetTools/Generic/DisposableList.cs
@@ -6,7 +6,7 @@ namespace XCLNetTools.Generic
     /// <summary>
     /// 带有 Dispose 的 List
     /// </summary>
-    public class DisposableList : List<IDisposable>, IDisposable
+    public class DisposableList<T> : List<T>, IDisposable where T : IDisposable
     {
         /// <summary>
         /// 释放内存
