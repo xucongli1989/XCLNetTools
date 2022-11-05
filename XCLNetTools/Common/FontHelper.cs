3fd4952cab70f889ff86ad40b4d4d3a1e85333e2
xucongli1989 <80213876@qq.com>
2022-11-05 15:09:20 +0800

    字体逻辑调整

--- XCLNetTools/Common/FontHelper.cs
+++ XCLNetTools/Common/FontHelper.cs
@@ -1,6 +1,9 @@
 ﻿using System.Collections.Generic;
 using XCLNetTools.Entity;
 using System.Linq;
+using System;
+using System.IO;
+using System.Collections.Concurrent;
 
 namespace XCLNetTools.Common
 {
@@ -10,58 +13,46 @@ namespace XCLNetTools.Common
     public static class FontHelper
     {
         /// <summary>
-        /// 中文字体信息
+        /// 获取当前所有字体信息（默认包含两个文件夹：C:\Windows\Fonts  和  C:\Users\XXX\AppData\Local\Microsoft\Windows\Fonts\）
         /// </summary>
-        public static readonly List<FontInfoEntity> CNFontInfoList = new List<FontInfoEntity>() {
-            new FontInfoEntity(){CNName="新细明体",ENName="PMingLiU" },
-            new FontInfoEntity(){CNName="细明体",ENName="MingLiU" },
-            new FontInfoEntity(){CNName="标楷体",ENName="DFKai-SB" },
-            new FontInfoEntity(){CNName="黑体",ENName="SimHei" },
-            new FontInfoEntity(){CNName="宋体",ENName="SimSun" },
-            new FontInfoEntity(){CNName="新宋体",ENName="NSimSun" },
-            new FontInfoEntity(){CNName="仿宋",ENName="FangSong" },
-            new FontInfoEntity(){CNName="楷体",ENName="KaiTi" },
-            new FontInfoEntity(){CNName="仿宋_GB2312",ENName="FangSong_GB2312" },
-            new FontInfoEntity(){CNName="楷体_GB2312",ENName="KaiTi_GB2312" },
-            new FontInfoEntity(){CNName="微软正黑体",ENName="Microsoft JhengHei" },
-            new FontInfoEntity(){CNName="微软雅黑体",ENName="Microsoft YaHei" },
-            new FontInfoEntity(){CNName="微软雅黑",ENName="Microsoft YaHei" },
-            new FontInfoEntity(){CNName="隶书",ENName="LiSu" },
-            new FontInfoEntity(){CNName="幼圆",ENName="YouYuan" },
-            new FontInfoEntity(){CNName="华文细黑",ENName="STXihei" },
-            new FontInfoEntity(){CNName="华文楷体",ENName="STKaiti" },
-            new FontInfoEntity(){CNName="华文宋体",ENName="STSong" },
-            new FontInfoEntity(){CNName="华文中宋",ENName="STZhongsong" },
-            new FontInfoEntity(){CNName="华文仿宋",ENName="STFangsong" },
-            new FontInfoEntity(){CNName="方正舒体",ENName="FZShuTi" },
-            new FontInfoEntity(){CNName="方正姚体",ENName="FZYaoti" },
-            new FontInfoEntity(){CNName="华文彩云",ENName="STCaiyun" },
-            new FontInfoEntity(){CNName="华文琥珀",ENName="STHupo" },
-            new FontInfoEntity(){CNName="华文隶书",ENName="STLiti" },
-            new FontInfoEntity(){CNName="华文行楷",ENName="STXingkai" },
-            new FontInfoEntity(){CNName="华文新魏",ENName="STXinwei" }
-        };
-
-        /// <summary>
-        /// 根据字体名称返回此名称对应的英文名称，如果是英文，则直接返回；如果有汉字，则从内置的静态列表中找英文名称，如果找不到，则返回空。
-        /// </summary>
-        public static string GetFontEnglishName(string fontName)
+        public static List<FontInfoEntity> GetAllFontInfoList()
         {
-            if (string.IsNullOrWhiteSpace(fontName))
+            var lst = new ConcurrentBag<FontInfoEntity>();
+            var pathList = new List<string>();
+            pathList.Add(Environment.GetFolderPath(Environment.SpecialFolder.Fonts));
+            pathList.Add(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), @"Microsoft\Windows\Fonts"));
+
+            pathList.ForEach(p =>
             {
-                return fontName;
-            }
-            //如果已经是英文名称，则直接返回
-            if (!XCLNetTools.StringHander.DataCheck.IsHasCHZN(fontName))
-            {
-                return fontName;
-            }
-            var font = CNFontInfoList.FirstOrDefault(k => k.CNName == fontName);
-            if (null == font)
-            {
-                return string.Empty;
-            }
-            return font.ENName;
+                XCLNetTools.FileHandler.ComFile.GetFolderFilesByRecursion(p).AsParallel().ForAll(fontPath =>
+                {
+                    try
+                    {
+                        var ttf = new System.Windows.Media.GlyphTypeface(new Uri(fontPath));
+                        var cnFontName = ttf.Win32FamilyNames[new System.Globalization.CultureInfo("zh-CN")];
+                        var enFontName = ttf.Win32FamilyNames[new System.Globalization.CultureInfo("en-US")];
+                        if (string.IsNullOrWhiteSpace(cnFontName) && string.IsNullOrWhiteSpace(enFontName))
+                        {
+                            return;
+                        }
+                        var info = new FontInfoEntity();
+                        info.Path = fontPath;
+                        info.FileNameWithoutExt = XCLNetTools.FileHandler.ComFile.GetFileName(fontPath, false);
+                        info.CNName = cnFontName;
+                        info.ENName = enFontName;
+                        info.FontName = string.IsNullOrWhiteSpace(cnFontName) ? enFontName : cnFontName;
+                        info.FontValue = enFontName;
+                        lst.Add(info);
+                    }
+                    catch
+                    {
+                        //
+                    }
+                });
+            });
+
+            //排序：包含中文排前面
+            return lst.OrderByDescending(k => XCLNetTools.StringHander.DataCheck.IsHasCHZN(k.FontName)).ThenBy(k => k.FontName).ToList();
         }
     }
 }
--- XCLNetTools/Entity/FontInfoEntity.cs
+++ XCLNetTools/Entity/FontInfoEntity.cs
@@ -13,13 +13,33 @@ namespace XCLNetTools.Entity
     public class FontInfoEntity
     {
         /// <summary>
-        /// 字体中文名称，如：宋体
+        /// 字体文件路径
+        /// </summary>
+        public string Path { get; set; }
+
+        /// <summary>
+        /// 字体文件名称（如：abc.ttf 中的 abc）
+        /// </summary>
+        public string FileNameWithoutExt { get; set; }
+
+        /// <summary>
+        /// 用于显示的字体中文名称，如：宋体
         /// </summary>
         public string CNName { get; set; }
 
         /// <summary>
-        /// 字体英文名称，如：SimSun
+        /// 用于显示的字体英文名称，如：SimSun
         /// </summary>
         public string ENName { get; set; }
+
+        /// <summary>
+        /// 用于显示的字体名称
+        /// </summary>
+        public string FontName { get; set; }
+
+        /// <summary>
+        /// 字体值，可以根据该字体值找到字体对象，默认为 ENName。（注意：不同的库在查询字体时对值的定义可能不一样，因此可以根据需要修改此值）
+        /// </summary>
+        public string FontValue { get; set; }
     }
 }
