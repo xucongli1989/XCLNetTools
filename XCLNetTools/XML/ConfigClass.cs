5ad487a8279d29105a82b1fdf18031f6f0be5041
xucongli1989 <80213876@qq.com>
2024-09-26 15:56:58 +0800

    update

--- XCLNetTools/XML/ConfigClass.cs
+++ XCLNetTools/XML/ConfigClass.cs
@@ -16,16 +16,19 @@ namespace XCLNetTools.XML
     public static class ConfigClass
     {
         /// <summary>
+        /// 获取统一的 AppSettings 前缀环境变量值
+        /// </summary>
+        public static string GetEnvXCLNetToolsAppSettingsPrefix()
+        {
+            return Environment.GetEnvironmentVariable("XCLNetToolsAppSettingsPrefix") ?? "".Trim();
+        }
+
+        /// <summary>
         /// 获取 appSettings 配置文件中的字符串值。注意：如果环境变量中包含 XCLNetToolsAppSettingsPrefix ，则默认会在 key 的前面自动加上这个变量值。用于在不修改代码的情况下，根据不同环境获取不同的 key 值。
         /// </summary>
         public static string GetConfigString(string key)
         {
-            var prefix = Environment.GetEnvironmentVariable("XCLNetToolsAppSettingsPrefix");
-            if (!string.IsNullOrWhiteSpace(prefix))
-            {
-                key = $"{prefix.Trim()}{key}";
-            }
-
+            key = $"{GetEnvXCLNetToolsAppSettingsPrefix()}{key}";
             var cfgName = (System.Collections.Specialized.NameValueCollection)System.Configuration.ConfigurationManager.GetSection("appSettings");
             return cfgName[key];
         }
