d66c5222e32b3d665bb15714b729e3819e2de8ac
xucongli1989 <80213876@qq.com>
2024-11-19 16:22:00 +0800

    添加Newtonsoft.Json序列化策略

---
+++ XCLNetTools/Entity/Serialize/FirstCharLowercaseNamingStrategy.cs
@@ -0,0 +1,23 @@
+﻿using Newtonsoft.Json.Serialization;
+
+namespace XCLNetTools.Entity.Serialize
+{
+    /// <summary>
+    /// Newtonsoft.Json 首字母小写策略
+    /// </summary>
+    public class FirstCharLowercaseNamingStrategy : NamingStrategy
+    {
+        protected override string ResolvePropertyName(string name)
+        {
+            if (string.IsNullOrWhiteSpace(name))
+            {
+                return name;
+            }
+            if (name.Length == 1)
+            {
+                return name.ToLower();
+            }
+            return char.ToLower(name[0]) + name.Substring(1);
+        }
+    }
+}
